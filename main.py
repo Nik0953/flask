643580eed49bc0319ba5327bb529a8f216446774
Nik0953 <7900953@gmail.com>
2022-08-11 12:59:50 +0300

    html+flask +hh.ru ver_1.0

--- main.py
+++ main.py
@@ -19,25 +19,30 @@ def run():
     # это будет список словарей-вакансий
     vac_lst = []
 
+    # чтение словаря регионов с кодами региона
+    input_file_name = 'data/regions.json'
+    regions_dict = get_cities_dict(input_file_name)
+
     if request.method == 'POST':
         # Получть данные формы
         req_form = {}
         req_form['key_words'] = request.form['query_str']
         req_form['were_to_find'] = request.form['where_to_find']
         req_form['n_days'] = request.form['n_days']
-        #print(req_form)
 
+        # запрос к hh.ru и сохранение вакансий
         output_file_name = 'data/vac_lst.json'
         vac_lst = get_vacancies(req_form, output_file_name)
-        vac_number = len(vac_lst)
-        return render_template('results.html', vac_lst=vac_lst, vac_number=vac_number)
+        # информация для публикации в html
+        vac_number = len(vac_lst)     # общее количество вакансий
+        # находим имя региона по номеру
+        vac_region = [reg for reg in regions_dict if regions_dict[reg] == req_form['were_to_find']][0]
+        #print(vac_region)
+
+        return render_template('results.html', vac_lst=vac_lst, req=req_form, vac_number=vac_number, vac_region=vac_region)
 
     elif request.method == 'GET':
         # выводим  html с формой запроса
-        # чтение словаря регионов с кодами региона
-        input_file_name = 'data/regions.json'
-        regions_dict = get_cities_dict(input_file_name)
-
         return render_template('req.html', regions_dict=regions_dict)
 
 
